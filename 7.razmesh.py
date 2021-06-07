44dde160e6b0c70b3bb41ea900ea7ca9f370329f
ScorpionMLG <85507036+ScorpionMLG@users.noreply.github.com>
2021-06-07 18:57:19 +0300

    Add files via upload

---
+++ 7.razmesh.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x = [1, 5, 10, 15, 20]
+y1 = [1, 7, 3, 5, 11]
+y2 = [i*1.2 + 1 for i in y1]
+y3 = [i*1.2 + 1 for i in y2]
+y4 = [i*1.2 + 1 for i in y3]
+plt.figure(figsize=(12, 7))
+plt.subplot(2, 2, 1)
+plt.plot(x, y1, '-')
+plt.subplot(2, 2, 2)
+plt.plot(x, y2, '--')
+plt.subplot(2, 2, 3)
+plt.plot(x, y3, '-.')
+plt.subplot(2, 2, 4)
+plt.plot(x, y4, ':')
+plt.show()
+fig, axs = plt.subplots(2, 2, figsize=(12, 7))
+axs[0, 0].plot(x, y1, '-')
+axs[0, 1].plot(x, y2, '--')
+axs[1, 0].plot(x, y3, '-.')
+axs[1, 1].plot(x, y4, ':')
+plt.show()
